bb32d934c934794d299769f05e4344e8d96292d3
VS <etconsilium@users.noreply.github.com>
2016-04-09 14:10:37 +0300

    random param

--- example.js
+++ example.js
@@ -3,6 +3,6 @@
 
 const setTimeline = require('settimeline').setTimeline;
 
-let $tl = setTimeline( function(data){console.log( data )}, {max:20, min:1, interval:10000})
+let $tl = setTimeline( function(data){console.log( data )}, {max:20, min:1, interval:10000, random: false})
 
 for (let i = 50; i > 0; i--) { $tl(i) }
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
 
   setTimeline: function(callback, rps, context) {
 
-    const _rps = {max:20, min:1, interval:1000}
+    const _rps = {max:20, min:1, interval:1000, random: 11}
     rps = Object.assign({}, _rps, rps)
     let _rq = { rps: rps
       , time_start:Date.now()
@@ -25,12 +25,15 @@ module.exports = {
         _rq.query_counter = 0
       }
 
-      let next_interval = Math.floor(
+      let next_interval = 
         Date.now() - _rq.time_last    //  возможное смещение
         + _rq.interval_counter * _rq.rps.interval
         + _rq.query_counter * _rq.rps.interval / (_rq.rps.max + 1)  //  интервалов на один больше
-        + Math.random() * _rq.rps.interval / (_rq.rps.max * 11)  //  произвольный коэффициент рандомности
-      );
+
+      if (parseInt(rps.random) === rps.random)
+        next_interval += Math.random() * _rq.rps.interval / (_rq.rps.max * _rq.rps.random)  //  произвольный коэффициент рандомности
+
+      next_interval = Math.floor( next_interval );
 
       setTimeout( function(){return (callback.apply(context,args))} , next_interval)
 
